249fbf08ac0f146cd7a858ad1fd5921d22bc06fa
The Lemon Club <picnicbox.pe@gmail.com>
2026-05-16 14:24:05 -0500

    feat: nuevo ícono de app con logo The Lemon Club

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tlc-v11';
+const CACHE = 'tlc-v12';
 const STATIC = ['./index.html', './styles.css', './app.js', './mascot.jpg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
